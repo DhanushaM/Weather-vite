738cc2db7deaacfe6a347f6b37495239cbaafa84
DhanushaM <dhanusham2601gmail.com>
2025-08-15 22:47:24 +0530

    Deploy Weather-vite manually

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-vite/'  // <--- add this line
+  base: '/Weather-vite/'  // <--- add this line
 })
 
